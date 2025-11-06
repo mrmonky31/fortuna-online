d8d85e1769289a70ba0916fb81627267c4b6119c
Monky <monky@mac.home>
2025-11-06 20:37:33 +0100

    socket collegato a render (token rimosso)

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,6 @@
-// src/socket.js
 import { io } from "socket.io-client";
 
-const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3001";
-
-export const socket = io(SERVER_URL, {
+export const socket = io("https://fortuna-online-server.onrender.com", {
   transports: ["websocket"],
   reconnection: true,
 });
