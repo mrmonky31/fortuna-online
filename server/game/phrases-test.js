11f4699efdb387a36d856a3a8041e4c281b46314
Monky <monky@mac.home>
2025-11-29 11:08:23 +0100

    fix definitivo: sincronizzazione forzata con GitHub

--- server/game/phrases-test.js
+++ server/game/phrases-test.js
@@ -2,7 +2,8 @@ export const testPhrases = [
 
 
 { category: "TUA MAMMA", text: "E' L'ANIMALA PIU' CAGNA  MIGLIORE" },
-{ category: "TUA SORELLA", text: "SI CHIAVA GLI ESTINTORI NEL CULO" },
+{ category: "TUA SORELLA", text: "HA LA FICA D'ACCIAIO E IL CULO DI BURRO" },
+
 
 
 ];
