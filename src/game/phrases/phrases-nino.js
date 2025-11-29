55c41303f3cc9245d8eea2ea07bac814a771005b
Monky <monky@mac.home>
2025-11-29 10:21:02 +0100

    fix definitivo: sincronizzazione forzata con GitHub

---
+++ src/game/phrases/phrases-nino.js
@@ -0,0 +1,8 @@
+export const testPhrases = [
+
+
+{ category: "TUA MAMMA", text: "A FARE I BOCCHINI COL CULO E LA MIGLIORE" },
+{ category: "TUA SORELLA", text: "SI CHIAVA GLI ESTINTORI NEL CULO" },
+
+
+];
---
+++ src/game/phrases/phrases-test.js
@@ -0,0 +1,8 @@
+export const testPhrases = [
+
+
+{ category: "TUA MAMMA", text: "E' L'ANIMALA PIU' CAGNA  MIGLIORE" },
+{ category: "TUA SORELLA", text: "SI CHIAVA GLI ESTINTORI NEL CULO" },
+
+
+];
