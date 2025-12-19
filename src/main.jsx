6591c05731f1a7bb8a451214e29e479a5f3aa011
Monky <monky@MacBook-Air-di-Monky.local>
2025-12-19 19:08:45 +0100

    fix definitivo: sincronizzazione forzata con GitHub

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+// 🔥 RIMOSSO React.StrictMode perché causa doppia esecuzione degli useEffect
+// in development, creando conflitti con il lucchetto del Time Challenge
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
